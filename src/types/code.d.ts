d9341e8ff24981d5684f7460f5a9c358bf6c7080
wmf <2397091734@qq.com>
2021-11-21 19:02:04 +0800

    add: 增加参数注释

--- src/types/code.d.ts
+++ src/types/code.d.ts
@@ -8,6 +8,10 @@ declare namespace StrongCode {
     }
     /**
      * @description 二维码边框参数
+     * @param color 边框颜色 默认没有边框 如果需要传数组 多个颜色支持渐变
+     * @param lineWidth 二维码边框的宽度
+     * @param opacity 二维码边框颜色透明度 默认不透明 0～1
+     * @param degree 二维码边框的角度
      */
     interface BorderCode {
         color: string[],
@@ -33,31 +37,32 @@ declare namespace StrongCode {
     }
     /**
      * @description 二维码文字绘制
-     * @param placement 文字的位置可选值 top bottom middle 默认 middle
+     * @param color 二维码绘制的文字颜色 默认黑色 如果需要修改 必须传数组 多种颜色表示可以渐变
+     * @param font 二维码绘制的文字 默认normal 20px system-ui
+     * @param content 要绘制的文字内容
      */
     interface CodeText {
         opacity?: number,
         font?: string,
-        placement?: string,
         color?: string[],
         content: string
     }
     /**
      * @description 生成二维码参数
-     * @param id canvasID 或者实列
-     * @param type 二维码绘制码点类型 可以使用自定义图片绘制
-     * @param size 画布canvas大小单位rpx
-     * @param code 生成二维码的code
-     * @param eyeSrc 如果 type设置为 custom 则次属性必传 码眼图片地址
-     * @param src 二维码背景图片地址
-     * @param padding 二维码内边距 默认0
-     * @param level 纠错等级 默认4
-     * @param bgColor 画布背景色 默认 #FFFFFF
-     * @param text 要绘制的文字参数 object
-     * @param color 二维码绘制的颜色 默认#000000 数组 可以传入多个颜色渐变 默认最多10种颜色
-     * @param img 二维码中间log图片配置
-     * @param border 二维码边框配置 
-     * @param ctx 自定义组件时需要 传this
+     * @param id String canvasID 或者实列 必传√
+     * @param type String 二维码绘制码点类型 可以使用自定义图片绘制 默认为 none 非必传
+     * @param size Number 画布canvas大小单位rpx 必传√
+     * @param code String/Number 生成二维码的code 必传√
+     * @param eyeSrc String 如果 type设置为 custom 则次属性必传 二维码的码眼图片地址 非必传
+     * @param src String 二维码背景图片地址 非必传
+     * @param padding Number 二维码内边距 默认0 单位rpx 非必穿
+     * @param level Number 纠错等级 默认4 非必传
+     * @param bgColor  String 画布背景色 默认 #FFFFFF 非必传
+     * @param text Object 要绘制的文字参数 不传则没有文字 非必传
+     * @param color Array 二维码绘制的颜色  数组 可以传入多个颜色渐变 默认最多10种颜色渐变 不传默认#000000 非比传
+     * @param img String 二维码中间log图片配置 不传则没有边框 非必传
+     * @param border Object 二维码边框配置 不传则没有边框 非必传
+     * @param ctx Object  自定义组件时需要 传this 必传√
      */
     interface BarCodePars {
         id: string | UniApp.CanvasContext,
@@ -77,6 +82,12 @@ declare namespace StrongCode {
     }
     /**
      * @description 保存二维码或者条形码为图片
+     * @param id String 保存画布的canvas-id 必传√
+     * @param width Number 保存画布的宽度 单位rpx 必传√
+     * @param height Number 保存画布的高度 单位rpx 必传√ß
+     * @param type String 保存图片的类型 默认 png 非必传
+     * @param quality Number 保存图片的质量 默认 1 可选 0～1 非必传
+     * @param ctx Object 当前上下文
      */
     interface SaveCanvasPars {
         id: string | UniApp.CanvasContext,
@@ -88,6 +99,13 @@ declare namespace StrongCode {
     }
     /**
      * @description 条形码生成参数
+     * @param id String 条形码canvas ID 必传√
+     * @param width Number 条形码宽度 单位rpx 必传√
+     * @param height Number 条形码高度 单位rpx 必传√
+     * @param type String 生成条形码的类型 默认CODE128 可选值 CODE39 EAN ITF MSI Codabar Pharmacode  非必传
+     * @param bgColor String 生成条形码的背景色 默认 #FFFFFF 非必传
+     * @param color Array 条形码的颜色 默认黑色不见变 传入多个颜色渐变 最多10种颜色 如果颜色一样则不会渐变
+     * @param ctx Object 自定义组建需要的上下文 必传√
      */
     interface OperationCodePars {
         id: string | UniApp.CanvasContext,
