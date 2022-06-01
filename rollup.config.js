8a0bb6117651671497cb6d9279422b3609058deb
mfwang <mfwang@pisx.com>
2022-06-01 14:25:24 +0800

    add: 增加版本号

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ export default {
         babel({
           exclude: 'node_modules/**'
         }),
-        // uglify(),
+        uglify(),
         typescript({
             tsconfig: getPath('./tsconfig.json')
         })
